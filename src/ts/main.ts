823e31d3705202afeaf42b63b5bdb1d6ef16a08a
Eraze86 <94078392+Eraze86@users.noreply.github.com>
2022-01-22 11:20:09 +0100

    Add files via upload

---
+++ src/ts/main.ts
@@ -0,0 +1,21 @@
+import { Movie } from "./modules/movie";
+
+window.onload = function(){
+    let main = new Main()
+    main.start();
+   
+}
+
+class Main{
+    start(){
+        let p = new Movie("Hajen", 13, true)
+    console.log(p)
+    }
+}  
+    
+
+
+
+
+  
+
---
+++ src/ts/modules/movie.ts
@@ -0,0 +1,13 @@
+export class Movie{
+    title: string;
+    age: number;
+    poster: boolean;
+
+    constructor(title: string, age:number, poster: boolean){
+        this.title = title;
+        this.age = age;
+        this.poster = poster;
+    }
+
+
+}
